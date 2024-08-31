88beb6df1f8a980fc15e0885d5aed8b6318965ee
Hidenori <hfu@users.noreply.github.com>
2024-08-31 17:09:34 +0900

    Update module.js

--- docs/module.js
+++ docs/module.js
@@ -1,5 +1,5 @@
-(async () => {
+export default (async () => {
   const maplibregl = await import('https://unpkg.com/maplibre-gl/dist/maplibre-gl.js');
   await import('https://hfu.github.io/h3j-test/h3j_h3t.js');
-  export default maplibregl.default;
+  return maplibregl.default;
 })();
